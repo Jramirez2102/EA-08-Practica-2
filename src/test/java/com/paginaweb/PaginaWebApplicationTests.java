c47d5e8f4cf95f42663c69b9a0c359fba8756f75
Jramirez2102 <gamer21rh@gmail.com>
2023-06-01 23:52:56 -0600

    Practica 02

---
+++ src/test/java/com/paginaweb/PaginaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paginaweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
